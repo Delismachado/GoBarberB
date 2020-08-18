85d79617c9d86fb5ea8796f93d82456ade90fd56
Derli Machado <dam.machado1@gmail.com>
2020-08-18 16:13:37 -0300

    Reset de senha.

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -5,6 +5,7 @@ import IUserTokensRepository from '@modules/users/repositories/IUserTokensReposi
 
 
 import UserToken from '../../infra/typeorm/entities/UserToken';
+import User from '@modules/users/infra/typeorm/entities/User';
 //import User from '@modules/users/infra/typeorm/entities/User';
 
 
@@ -23,6 +24,13 @@ class FakeUserTokensRepository implements IUserTokensRepository {
     this.userTokens.push(userToken);
             
       return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(findToken => findToken.token === token);
+
+    return userToken;
+
   }
     
 }
---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,48 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUserRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+import ResetPasswordService from './ResetPasswordService';
+
+let fakeUserRepository: FakeUserRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let resetPassword: ResetPasswordService;
+
+describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+     fakeUserRepository = new FakeUserRepository();
+     fakeUserTokensRepository = new FakeUserTokensRepository();        
+
+     resetPassword = new ResetPasswordService(
+        fakeUserRepository,
+        fakeUserTokensRepository,
+      );
+
+
+  });
+
+    it('should be able to reset passwordl', async () => {
+        
+        const user = await fakeUserRepository.create({
+          name: 'teste',
+          email: 'teste@teste.com',
+          password: '123123',
+        });
+
+        const {token} = await fakeUserTokensRepository.generate(user.id);
+
+
+        await resetPassword.execute({
+          password: '123456',
+          token,
+        });
+
+        const updatedUser = await fakeUserRepository.findById(user.id);
+
+        expect(updatedUser?.password).toBe('123456');
+       
+    });  
+    
+         
+});
+
