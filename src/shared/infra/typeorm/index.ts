ded2170c3b8688384894c2ec651a07873009b237
Derli Machado <dam.machado1@gmail.com>
2022-01-23 12:57:45 -0300

    Adds: postgres heroku configuration.

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,2 +1,19 @@
-import { createConnections } from 'typeorm';
-createConnections();
+import { createConnections, createConnection } from 'typeorm';
+if (process.env.DATABASE_URL) {
+    createConnection({
+        "name": "default",
+        "type": "postgres",
+        "url": process.env.DATABASE_URL,
+        "entities": [
+            "./src/modules/**/infra/typeorm/entities/*.ts"
+        ],
+        "migrations": [
+            "./src/shared/infra/typeorm/migrations/*.ts"
+        ],
+        "cli": {
+            "migrationsDir": "./src/shared/infra/typeorm/migrations"
+        }
+    });
+} else {
+    createConnections();
+}
