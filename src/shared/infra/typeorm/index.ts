f9120f383ca5c5435faedc74788de13b0e612fa6
Derli Machado <dam.machado1@gmail.com>
2022-01-23 15:20:04 -0300

    FIx: missing ssl config.

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,22 +3,23 @@ import { createConnections, createConnection } from 'typeorm';
 async function connect(): Promise<void> {
     if (process.env.DATABASE_URL) {
         const connections = await createConnections([{
-            "name": "default",
-            "type": "postgres",
-            "url": process.env.DATABASE_URL,
-            "synchronize": false,
-            "logging": false,
-            "extra": {
-                "ssl": true,
+            name: "default",
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            synchronize: false,
+            logging: false,
+            ssl: { rejectUnauthorized: false },
+            extra: {
+                ssl: true,
             },
-            "entities": [
+            entities: [
                 "./dist/modules/**/infra/typeorm/entities/*.js"
             ],
-            "migrations": [
+            migrations: [
                 "./dist/shared/infra/typeorm/migrations/*.js"
             ],
-            "cli": {
-                "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+            cli: {
+                migrationsDir: "./dist/shared/infra/typeorm/migrations"
             }
         }]);
         connections.forEach(c => c.runMigrations());
